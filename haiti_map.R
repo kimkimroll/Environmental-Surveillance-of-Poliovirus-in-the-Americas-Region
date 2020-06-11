6cfb651989d84de2f53a7ed2057abd9c4c3324a9
Kim <nvj5@cdc.gov>
2020-06-11 15:03:15 -0400

    Create Haiti terrain map
    Terrain map

---
+++ haiti_map.R
@@ -0,0 +1,63 @@
+setwd("//cdc.gov/project/CCID_NCIRD_DVD_PPLB/_PMDDL/Kim/R/envs_data")
+
+install.packages("ggplot2")
+install.packages("ggmap")
+install.packages("maps")
+install.packages("mapproj")
+install.packages("mapdata")
+install.packages("rgeos")
+install.packages("maptools")
+install.packages("sp")
+install.packages("raster")
+install.packages("rgdal")
+install.packages("dismo")
+
+
+require(ggplot2)
+require(ggmap)
+require(maps)
+require(mapproj)
+require(mapdata)
+require(rgeos)
+require(maptools)
+require(sp)
+require(raster)
+require(rgdal)
+require(dismo)
+
+#making basic maps with ggmap
+base = get_map(location=c(-105,0,-60,30), zoom=4, maptype="terrain-background")
+
+map1=ggmap(base)
+
+map1
+map1 + geom_point(data=k, aes(x=long, y=lat, fill=site), color="red", cex=2.5)
+
+
+
+
+#load envs data with site coordinates
+envs<-read.csv("allsamples_envsdata_04242020.csv", header = TRUE)
+
+
+
+#TERRAIN MAP FOR HAITI
+base = get_map(location=c(-74.5,18,-71,20), zoom=10, maptype="terrain-background")
+
+map1=ggmap(base)
+
+map1
+map1 + geom_point(data=envs, aes(x=long, y=lat, fill=site), color="red", cex=2.5, show.legend = FALSE)+
+  theme(legend.position = "none",
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank())
+
+
+
+#EXPORT IMAGE WITH CLEAR RESOLUTION
+
+tiff("imagekimhpan.tif", res=800, compression = "lzw", height=5, width=5, units="in")
+
+
